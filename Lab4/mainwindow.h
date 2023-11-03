415ee8c37002c3c8c8ebd09d6ae11c9625065ec8
bzimm005 <bzimm005@odu.edu>
2023-11-03 16:38:43 -0400

    fixed slider bugs and improved some things

---
+++ Lab4/mainwindow.h
@@ -0,0 +1,45 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFileDialog>
+#include <QPixmap>
+#include <QMessageBox>
+#include <QLabel>
+#include <QUdpSocket>
+#include <iostream>
+#include <QBuffer>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+    void updateContrastValue(int value);
+    void updateBrightnessValue(int value);
+    void updateBrightness(int value);
+    void updateContrast(int value);
+    void saveImage();
+    void transferOriginialImage();
+    void transferEditedImage();
+    void OverlayToggle();
+
+private:
+    Ui::MainWindow *ui;
+    int currentBrightness = 0;
+    int currentContrast = 0;
+    QPixmap modifiedImage;
+    QPixmap originalImage;
+    int port = 12345;
+    QString ipaddress = "192.168.1.229";
+};
+#endif // MAINWINDOW_H
